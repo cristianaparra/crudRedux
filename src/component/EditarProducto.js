031115ecaca4e6621d63e46ba8bd3f80bae3dad4
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-13 19:22:11 -0300

    creando lista edicion de productos y listado

--- src/component/EditarProducto.js
+++ src/component/EditarProducto.js
@@ -2,7 +2,42 @@ import React from 'react';
 
 const EditarProducto = () => {
     return (  
-        <div> desde EDITAR productos</div>
+        <div className='row justify-content-center'>
+            <div className='col-md-8'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='text-center mb-4 font-weight-bold'>
+                            Editar Producto
+                        </h2>
+                        <form>
+                            <div className='form-group'>
+                                <label>Nombre Producto</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    placeholder='NombreProducto'
+                                    name='nombre'
+                                />
+                            </div>
+
+                            <div className='form-group'>
+                                <label>Precio Producto</label>
+                                <input
+                                    type='number'
+                                    className='form-control'
+                                    placeholder='NombreProducto'
+                                    name='precio'
+                                />
+                            </div>
+                            <button
+                                type='submit'
+                                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+                            >Guardar Cambios</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
