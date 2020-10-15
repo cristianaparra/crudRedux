1167cde99f8e3872a6d56f579d1a06696ea5efd1
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-14 23:27:44 -0300

    insertar prodcutos api

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,16 +3,22 @@ import {
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR
 } from '../types'
-
+import clienteAxios from '../config/axios'
 //crear nuevos prodcutos
 
 export function crearNuevosProductosAction(producto) {
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch(agregarProducto());
 
         try {
+            //insertar en la api
+            await clienteAxios.post('/productos', producto);
+
+            //si todo sale bien, actualizar el state
             dispatch(agregarProductoExito(producto))
         } catch (error) {
+            console.log(error);
+            //si hay un error cambiar el state 
             dispatch(agregarProductoError(true))
 
         }
@@ -21,7 +27,7 @@ export function crearNuevosProductosAction(producto) {
 
 const agregarProducto = () => ({
     type: AGREGAR_PRODUCTO,
-    payload:true
+    payload: true
 })
 
 //si el producto se guarda ene la base de datos
@@ -30,7 +36,7 @@ const agregarProductoExito = producto => ({
     payload: producto
 })
 // si hubo un error
-const agregarProductoError = estado => ({ 
-    type:AGREGAR_PRODUCTO_ERROR,
+const agregarProductoError = estado => ({
+    type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
 })
